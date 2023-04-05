8c6505aec4ff7d9ed1d13a9849753fa632759eec
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-05 18:39:59 -0400

    remove button removes from state only

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,33 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../store/cart";
 
 function CartItem({ item }) {
+  const dispatch = useDispatch();
   const [count, setCount] = useState(item.count);
 
   useEffect(() => {
     setCount(item.count);
   }, [item.count]);
+  const handleClick = () => {
+    console.log(item);
+    dispatch(removeFromCart(item.id));
+  };
 
   return (
     <li className="cart-item">
       <div className="cart-item-header">{item.name}</div>
       <div className="cart-item-menu">
-        <input
-          type="number"
-          value={count}
-        />
-        <button
-          className="cart-item-button"
-        >
-          +
-        </button>
-        <button
-          className="cart-item-button"
-        >
-          -
-        </button>
-        <button
-          className="cart-item-button"
-        >
+        <input type="number" value={count} />
+        <button className="cart-item-button">+</button>
+        <button className="cart-item-button">-</button>
+        <button className="cart-item-button" onClick={handleClick}>
           Remove
         </button>
       </div>
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,5 @@
 const ADD_TO_CART = "cart/addToCart";
+const REMOVE_FROM_CART = "cart/removeFromCart";
 
 export const addToCart = (id) => {
   return {
@@ -7,15 +8,27 @@ export const addToCart = (id) => {
   };
 };
 
+export const removeFromCart = (id) => {
+  return {
+    type: REMOVE_FROM_CART,
+    id: id,
+  };
+};
+
 export default function cartReducer(state = {}, action) {
   switch (action.type) {
     case ADD_TO_CART:
-      const newState = {
+      const newState1 = {
         ...state,
         [action.id]: { id: action.id, count: 1 },
       };
-      console.log(newState);
-      return newState;
+      return newState1;
+    case REMOVE_FROM_CART:
+      const newState2 = state;
+      console.log("newState2", newState2);
+      delete newState2[action.id];
+      console.log("after newState2", newState2);
+      return newState2;
     default:
       return state;
   }
