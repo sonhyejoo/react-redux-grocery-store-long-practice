bd4d609940dfac113f0aa85d89d98adf02426790
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-05 17:30:44 -0400

    plus button adds to cart state

--- src/components/ProduceList/ProduceDetails.js
+++ src/components/ProduceList/ProduceDetails.js
@@ -1,17 +1,23 @@
+import { addToCart } from "../../store/cart";
+import { useDispatch } from "react-redux";
+
 function ProduceDetails({ produce }) {
   const cartItem = {};
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(addToCart(produce.id));
+  };
 
   return (
     <li className="produce-details">
       <span>{produce.name}</span>
       <span>
-        <button
-          className={"like-button" + (produce.liked ? " selected" : "")}
-        >
+        <button className={"like-button" + (produce.liked ? " selected" : "")}>
           <i className={"fas fa-heart"} />
         </button>
         <button
           className={"plus-button" + (cartItem ? " selected" : "")}
+          onClick={handleClick}
         >
           <i className="fas fa-plus" />
         </button>
